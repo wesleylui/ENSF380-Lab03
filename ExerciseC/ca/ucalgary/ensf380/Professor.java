a87e3334a485fcc63af3872e6d4cfe7652292460
Wesley Lui <wesley.lui2@ucalgary.ca>
2024-07-16 14:50:07 -0600

    update professor class

--- ExerciseC/ca/ucalgary/ensf380/Professor.java
+++ ExerciseC/ca/ucalgary/ensf380/Professor.java
@@ -1,8 +1,36 @@
 /*
  * @author Aiden Lambert, Wesley Lui 
  */
-public class Professor {
+public class Professor extends Person {
 	// Member variables
 	private String teacherNumber;
-	private double Salary;
+	private double salary;
+
+	// Constructor
+	public Professor(String name, String phoneNumber, String emailAddress, Address address, String teacherNumber,
+			double salary) {
+		super(name, phoneNumber, emailAddress, address);
+		this.teacherNumber = teacherNumber;
+		this.salary = salary;
+	}
+
+	// Getters
+	public String getTeacherNumber() {
+		return this.teacherNumber;
+	}
+
+	public double getSalary() {
+		return this.salary;
+	}
+
+	// Setters
+	public void setTeacherNumber(String teacherNumber) {
+		this.teacherNumber = teacherNumber;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	// No additional Methods
 }
